004754a56a159ceb53e27cbac6a72c0d1c745203
Fernando Rodriguez <fernando@keepcoding.io>
2016-09-21 16:07:19 -0700

    Initial Commit

---
+++ AutoLayout-4-ImageViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout-4-ImageViews
+//
+//  Created by Fernando Rodríguez Romero on 9/21/16.
+//  Copyright © 2016 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
